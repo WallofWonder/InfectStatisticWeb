2eb0560cbce0622adb3d7f40280ccb4b793626b1
ZYF <m13110683916@163.com>
2020-03-09 20:05:50 +0800

    init

---
+++ backend/src/test/java/com/infect/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infect.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
